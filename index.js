9e371e3cd70591e3498da8fd10e5a4b2337f86ab
parikshitsharma70 <parikshitsharma70@gmail.com>
2019-02-12 23:33:52 -0500

    Added the person constructor

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ Women have been assigned odd numbers : [1, 3, 5, ..]
 
 */
 
+//Module imports
+var util = require('util')
+
 //File imports
 var input = require('./input.json')
 
@@ -19,32 +22,89 @@ var menPref = input.men
 var womenPref = input.women
 
 //Global variables
-var n, mlen, wlen;
+var n, mlen, wlen
+var persons = []
+
+function Person(id, gender){
+    var index = 0
+
+    this.id = id
+    this.gender = gender
+    this.preference = []
+    this.match = null
+
+    this.rank = function(x){
+        for(var i = 0; i<this.preference.length ; i++){
+            if(this.preference[i] == x) return i
+        return this.preference.length + 1
+        }
+    }
+    this.prefers = function(x) {
+        return this.rank(x) < this.rank(this.match);
+    }
+ 
+    this.nextCandidate = function() {
+        if (index >= this.preference.length) return null;
+        return this.preference[index++];
+    }
+ 
+    this.engageTo = function(p) {
+        if (p.match) p.match.match = null;
+        p.match = this;
+        if (this.match) this.match.match = null;
+        this.fiance = p;
+    }
+ 
+    this.swapWith = function(p) {
+        console.log("%s & %s swap partners", this.name, p.name);
+        var thisFiance = this.fiance;
+        var pFiance = p.fiance;
+        this.engageTo(pFiance);
+        p.engageTo(thisFiance);
+    }
+}
 
 //Function to initalize the algorithm
 async function initialize() {
 
     if (menPref == "" || womenPref == "") {
-        console.error("Incorrect format of input file () /n Process exited with code : ", code)
+        console.error("Incorrect format of input file () \n Process exited with code : ", code)
         process.exitCode = 1
     }
-
     mlen = Object.keys(menPref).length
     wlen = Object.keys(womenPref).length
-    console.log('Number of men : ' + mlen)
-    console.log('Number of women : ' + wlen)
+    console.log('Men Pref : \n' + (util.inspect(menPref, { showHidden: false, depth: null })))
+    console.log('Women Pref : \n' + (util.inspect(womenPref, { showHidden: false, depth: null })))
 
 
     if (mlen == wlen) {
         console.log('Input file is in correct format!')
         n = mlen
         console.log('Size of input : ' + n)
-
+        matchPref()
     }
     else {
-        console.error("Incorrect format of input file (Number of men == Number of women). /n Process exited with code : ", code)
+        console.error("Incorrect format of input file (Number of men == Number of women). \n Process exited with code : ", code)
     }
 
+
+
+}
+
+async function matchPref(){
+    var mPref = menPref
+    var wPref = womenPref
+    for(var i=0; i < 2 * Object.keys(mPref).length; i = i + 2){
+        var key = Object.keys(mPref)[i/2]
+        console.log('Key : '+key)
+        var values = menPref[i].split(':')
+        var pref = JSON.stringify(values[0])
+        var y = pref.replace(/['"]+/g, '')
+        var x = y.split(',')
+        x.forEach(el =>{
+            console.log(el.trim())
+        })
+    }
 }
 
 //Call function
